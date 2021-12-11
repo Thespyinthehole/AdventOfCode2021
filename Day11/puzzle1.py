be86107f74c1a2c2d1ab6fbea0617c87e3d45f51
Thespyinthehole <joshuaaubrey@talktalk.net>
2021-12-11 22:51:06 +0000

    Day11

---
+++ Day11/puzzle1.py
@@ -0,0 +1,40 @@
+def energy_burst(x, y, grid, searched):
+    if ((x, y) in searched) or x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]):
+        return
+    grid[x][y] = grid[x][y] + 1
+    if grid[x][y] == 10:
+        searched.append((x, y))
+        energy_burst(x - 1, y - 1, grid, searched)
+        energy_burst(x - 1, y, grid, searched)
+        energy_burst(x - 1, y + 1, grid, searched)
+        energy_burst(x, y - 1, grid, searched)
+        energy_burst(x, y, grid, searched)
+        energy_burst(x, y + 1, grid, searched)
+        energy_burst(x + 1, y - 1, grid, searched)
+        energy_burst(x + 1, y, grid, searched)
+        energy_burst(x + 1, y + 1, grid, searched)
+
+
+def puzzle(data):
+    grid = []
+    for line in data:
+        line = line.replace('\n', '')
+        grid.append([int(x) for x in line])
+
+    total = 0
+    for i in range(100):
+        for x in range(len(grid)):
+            for y in range(len(grid[x])):
+                search = []
+                energy_burst(x, y, grid, search)
+
+        for x in range(len(grid)):
+            for y in range(len(grid[x])):
+                if grid[x][y] > 9:
+                    total = total + 1
+                    grid[x][y] = 0
+    print("Answer: " + str(total))
+
+
+data = open(__file__.replace('.py', 'input'))
+puzzle(data.readlines())
---
+++ Day11/puzzle2.py
@@ -0,0 +1,44 @@
+def energy_burst(x, y, grid, searched):
+    if ((x, y) in searched) or x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]):
+        return
+    grid[x][y] = grid[x][y] + 1
+    if grid[x][y] == 10:
+        searched.append((x, y))
+        energy_burst(x - 1, y - 1, grid, searched)
+        energy_burst(x - 1, y, grid, searched)
+        energy_burst(x - 1, y + 1, grid, searched)
+        energy_burst(x, y - 1, grid, searched)
+        energy_burst(x, y, grid, searched)
+        energy_burst(x, y + 1, grid, searched)
+        energy_burst(x + 1, y - 1, grid, searched)
+        energy_burst(x + 1, y, grid, searched)
+        energy_burst(x + 1, y + 1, grid, searched)
+
+
+def puzzle(data):
+    grid = []
+    for line in data:
+        line = line.replace('\n', '')
+        grid.append([int(x) for x in line])
+
+    total = 0
+    for i in range(1000):
+        count = 0
+        for x in range(len(grid)):
+            for y in range(len(grid[x])):
+                search = []
+                energy_burst(x, y, grid, search)
+
+        for x in range(len(grid)):
+            for y in range(len(grid[x])):
+                if grid[x][y] > 9:
+                    count = count + 1
+                    grid[x][y] = 0
+        if count == 100:
+            total = i + 1
+            break
+    print("Answer: " + str(total))
+
+
+data = open(__file__.replace('.py', 'input'))
+puzzle(data.readlines())
